434851a15cd02298bbfd2c8659ac3c4910011afc
EC2 Default User <ec2-user@ip-172-31-42-201.ap-northeast-1.compute.internal>
2024-04-25 12:27:41 +0000

    240425-1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    posts = Post.find(params[:id])
+    posts.destroy
+    redirect_to '/posts'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :sentence)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root to: "home#top"
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
   devise_for :users
-  resouces :users
+  resources :users
+  resources :posts, only: [:new, :create, :index, :show, :destroy]
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
+
 end
