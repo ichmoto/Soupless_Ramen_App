37b0e5378e014aa8078cb081455575b469d15608
EC2 Default User <ec2-user@ip-172-31-42-201.ap-northeast-1.compute.internal>
2024-05-14 13:08:25 +0000

    240514-1

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,9 @@
-class Admins::PostsController < ApplicationController
-    before_action :authenticate_admin!
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to admin_dashboards_path, notice: '投稿を削除しました。'
-    end
+class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_dashboards_path, notice: '投稿を削除しました。'
+  end
+
 end
