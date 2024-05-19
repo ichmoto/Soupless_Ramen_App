1c724b14c8d84455b817147c15d599da28f6509f
EC2 Default User <ec2-user@ip-172-31-42-201.ap-northeast-1.compute.internal>
2024-05-19 14:31:57 +0000

    240519-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:favorite]
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -6,4 +7,15 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def favorite
+    favorite = Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorite)
+  end
+
+private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,14 @@ Rails.application.routes.draw do
     resources :users, :posts
   end
   devise_for :users
-  resources :users
+  resources :users do
+   member do
+    get :favorite
+   end
+  end
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
-    resource :favorite, only: [:create, :destroy]
+    resources :favorite
   end
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
